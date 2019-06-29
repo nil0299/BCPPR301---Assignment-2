52cc46a12c7db4744c47ef852de3210518a313cd
nil0299 <36713118+nil0299@users.noreply.github.com>
2019-06-29 18:04:44 +1200

    Add class_finder.py

---
+++ PlantUML/module_builder/class_finder.py
@@ -0,0 +1,23 @@
+from .relationship_finder import RelationshipFinder
+
+class ClassFinder:
+
+    class_list = []
+
+    @classmethod
+    def find_classes(cls, class_content_list, relationship_content):
+        for class_info in class_content_list:
+            class_name = class_info.split(' ')[1]
+            info = class_info.split("\n")
+            attributes = [line for line in info if line.find(":")
+                          is not -1 and line.find("(") is -1]
+            methods = [line for line in info if line.find("(") is not -1]
+            relationships = [RelationshipFinder.find_relationship(relationship,
+                                                                  class_name)
+                             for relationship in
+                             relationship_content.split("\n") if
+                             RelationshipFinder.find_relationship(relationship,
+                                                                  class_name)]
+            new_tuple = (class_name, attributes, methods, relationships)
+            cls.class_list.append(new_tuple)
+        return cls.class_list
