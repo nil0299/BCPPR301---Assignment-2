9818b412f483e9bdf41207f290f12db7a8aef95b
nil0299 <36713118+nil0299@users.noreply.github.com>
2019-07-01 06:49:04 +1200

    Update class_builder.py

--- PlantUML/module_builder/class_builder.py
+++ PlantUML/module_builder/class_builder.py
@@ -5,66 +5,46 @@ from .relationship import Relationship
 
 class ClassBuilder:
 
-    """Create an instance of the object ClassBuilder
-    that contains a list of the objects attributes, methods
-    and relationships"""
-
     def __init__(self):
         self.name = ""
-        self.attributes = ""
-        self.methods = ""
-        self.relationships = []
         self.all_my_attributes = []
         self.all_my_methods = []
         self.all_my_parent_classes = []
         self.all_my_composite_classes = []
         self.all_my_associated_classes = []
 
-    def add_class_attributes(self):
-        for an_attribute in self.attributes:
-            new_a_name = an_attribute.split(":")[0]
-            new_a_return = an_attribute.split(":")[1]
-            new_a = Attribute(new_a_name, new_a_return)
+    def add_class_attributes(self, new_attributes):
+        for an_attribute in new_attributes:
+            new_a = Attribute(an_attribute.split(":")[0],
+                              an_attribute.split(":")[1])
             self.all_my_attributes.append(new_a)
 
-    def add_class_methods(self):
-        for a_method in self.methods:
-            new_m_name = a_method.split("(")[0]
-            new_m_return = a_method.split(")")[1]
-            new_m_input = a_method[a_method.find("(") + 1:a_method.find(")")]
-            new_m = Method(new_m_name, new_m_return, new_m_input)
+    def add_class_methods(self, new_methods):
+        for a_method in new_methods:
+            new_m = Method(a_method.split("(")[0],
+                           a_method.split(")")[1],
+                           a_method[
+                           a_method.find("(") + 1:a_method.find(")")])
             self.all_my_methods.append(new_m)
 
-    def add_relationships(self):
-        for a_relationship in self.relationships:
-            if "extends" in a_relationship:
-                new_relationship = Relationship(a_relationship)
-                self.all_my_parent_classes.append(new_relationship)
-            if "comp" in a_relationship:
-                new_relationship = Relationship(a_relationship)
-                self.all_my_composite_classes.append(new_relationship)
-            if "assos" in a_relationship:
-                new_relationship = Relationship(a_relationship)
-                self.all_my_associated_classes.append(new_relationship)
+    def add_relationships(self, new_relationships):
+        for a_relationship in new_relationships:
+            new_relationship = Relationship(a_relationship, self)
+            new_relationship.add_relationship()
 
-    def build_class(
-            self, new_name, new_attributes, new_methods, new_relationships):
-        self.name = new_name
-        self.attributes = new_attributes
-        self.methods = new_methods
-        self.relationships = new_relationships
-        self.add_class_attributes()
-        self.add_class_methods()
-        self.add_relationships()
+    def build_class(self, args):
+        self.name = args[0]
+        self.add_class_attributes(args[1])
+        self.add_class_methods(args[2])
+        self.add_relationships(args[3])
 
-    def print_class(self):
+    def __str__(self):
         string = ""
         string += f"class {self.name}"
         if len(self.all_my_parent_classes) > 0:
             for a_class in self.all_my_parent_classes:
                 string += f"({a_class})"
-        string += ":\n"
-        string += "\n    def __init__(self):\n"
+        string += ":\n\n    def __init__(self):\n"
         for x in self.all_my_attributes:
             string += f"{x}"
         if len(self.all_my_composite_classes) > 0:
@@ -74,3 +54,6 @@ class ClassBuilder:
         for x in self.all_my_methods:
             string += f"{x}"
         return string
+
+    def print_class(self):
+        return self.__str__()
